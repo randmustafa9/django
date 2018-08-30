021c7da381fef3b666d3b261603086cdb7376625
zainab imad <zainabimad71@gmail.com>
2018-08-30 20:40:48 +0300

    static and media

--- sponsors/sponsors/settings.py
+++ sponsors/sponsors/settings.py
@@ -119,4 +119,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'images/static/media')
+MEDIA_URL='/media/'
