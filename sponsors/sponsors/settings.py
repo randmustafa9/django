38b1e387df688908dde27810c35cda680c8af817
zainab imad <zainabimad71@gmail.com>
2018-08-30 20:56:21 +0300

    edit static and media

--- sponsors/sponsors/settings.py
+++ sponsors/sponsors/settings.py
@@ -131,5 +131,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-MEDIA_ROOT=os.path.join(BASE_DIR,'images/static/media')
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
